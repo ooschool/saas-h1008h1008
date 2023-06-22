f3a5677943334741d9f4f84a7007ecd176294d03
h1008h1008 <103942745+h1008h1008@users.noreply.github.com>
2023-06-22 13:53:42 +0800

    beauti，add random prompts functiondd random prompts function

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import openai
 import re
 import requests
 import base64
+import json
 load_dotenv()
 # NB: host url is not prepended with \"https\" nor does it have a trailing slash.
 STABILITY_HOST = os.getenv('ALLOWED_EXTENSIONS')
@@ -44,126 +45,119 @@ def upload_file():
 
 @app.route('/', methods=['POST'])
 def uploading_file():
-    file = request.files['file'].read()
-    thought = request.form.get('thought')
     flag = request.form.get('flag')
-    string = "IDEA:只要生成英文的"+thought+"，prompt細節越多越好，並將這些從最重要到不重要排列，請直接給我完整的prompt，不要生成其他東西，請不要加入不相關的prompt，只要生成一個英文prompt"
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "system", "content": "You are a helpful assistant which gernarate prompt."},
-            {"role": "user", "content": '''Stable Diffusion is an AI art generation model similar to DALLE-2.
-    Here are some prompts for generating art with Stable Diffusion. 
-
-    Example:
-
-    - portait of a homer simpson archer shooting arrow at forest monster, front game card, drark, marvel comics, dark, intricate, highly detailed, smooth, artstation, digital illustration
-    - pirate, concept art, deep focus, fantasy, intricate, highly detailed, digital painting, artstation, matte, sharp focus, illustration
-    - ghost inside a hunted room, art by lois van baarle and loish and ross tran and rossdraws and sam yang and samdoesarts and artgerm, digital art, highly detailed, intricate, sharp focus, Trending on Artstation HQ, deviantart, unreal engine 5, 4K UHD image
-    - red dead redemption 2, cinematic view, epic sky, detailed, concept art, low angle, high detail, warm lighting, volumetric, godrays, vivid, beautiful, trending on artstation
-    - a fantasy style portrait painting of rachel lane / alison brie hybrid in the style of francois boucher oil painting unreal 5 daz. rpg portrait, extremely detailed artgerm
-    - athena, greek goddess, claudia black, art by artgerm and greg rutkowski and magali villeneuve, bronze greek armor, owl crown, d & d, fantasy, intricate, portrait, highly detailed, headshot, digital painting, trending on artstation, concept art, sharp focus, illustration
-    - closeup portrait shot of a large strong female biomechanic woman in a scenic scifi environment, intricate, elegant, highly detailed, centered, digital painting, artstation, concept art, smooth, sharp focus, warframe, illustration
-    - ultra realistic illustration of steve urkle as the hulk, intricate, elegant, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration
-    - portrait of beautiful happy young ana de armas, ethereal, realistic anime, trending on pixiv, detailed, clean lines, sharp lines, crisp lines, award winning illustration, masterpiece, 4k, eugene de blaas and ross tran, vibrant color scheme, intricately detailed
-    - A highly detailed and hyper realistic portrait of a gorgeous young ana de armas, lisa frank, trending on artstation, butterflies, floral, sharp focus, studio photo, intricate details, highly detailed, alberto seveso and geo2099 style
-
-    Prompts should be written in English, excluding the artist name, and include the following rule:
-
-    - Follow the structure of the example prompts. This means Write a description of the scene, followed by modifiers divided by commas to alter the mood, style, lighting, and more, excluding the artist name, separated by commas. place a extra commas. and place the extra Chinese translation for prompt at the end of each prompt.
-
-    I want you to write me a list of detailed prompts exactly about the IDEA follow the rule at most 1  every time and don't generate other sentense only prompt. tell me ok if you get my point'''},
-            {"role": "system", "content": "Sure, I got your point."},
-            {"role": "user", "content":string},
-        ]
-    )
-    thought = response['choices'][0]['message']['content']
-    if ':' in thought:
-        thought = thought[thought.index(':'):]
-    paragraphs = thought.split('-')
-    # Find the longest paragraph
-    longest_paragraph = max(paragraphs, key=len)
-    text = re.sub(r'\([^)]*\)', '', longest_paragraph)
-    print(text)
-    if flag == '0':
-        url = "https://api.mokker.ai/v1/replace-background"
-        files = {
-            "image": file
-        }
-        payload = {
-            "pos_prompt": text,
-            "number_of_images": "1"
-        }
-        headers = {
-            "Authorization": "Bearer " + MOKKER_KEY,
-            "accept": "application/json"
-        }
-        response = requests.post(
-        url = url,
-        headers = headers,
-        data = payload,
-        files = files
+    if flag == '2':
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant who generate random products prompt "},
+                {"role": "user", "content": "random products"},
+                {"role": "assistant", "content": '''1. A luxury lotion bottle<br>2. The toy  robot <br>3. Delicious salmon<br>4.modern TV<br>'''},
+                {"role": "user", "content": "random products"},
+                {"role": "assistant", "content": '''1. A smart watch with fitness tracking features<br>2. A portable Bluetooth speaker<br>3. A vegan leather handbag<br>4. An automatic milk frother for coffee and lattes<br>'''},
+                {"role": "user", "content": "random products"},
+            ]
         )
-        if response.status_code == 200:
-            # Retrieve the generated image data
-            try:
-                # Retrieve the generated images list
-                images = response.json().get('images', [])
-
-                if images:
-                    for image in images:
-                        # Retrieve the generated image data
-                        image_data = image.get('image', {}).get('data')
-
-                        if image_data:
-                            # Decode the base64 image data
-                            image_bytes = base64.b64decode(image_data)
-
-                            # Specify the file path and name
-                            script_path = os.path.abspath( __file__ )
-                            localtime = time.localtime()
-                            result = time.strftime("%Y%m%d%I%M%S%p", localtime)
-                            filename = "/static/images/" + str(result) + ".jpg"
-                            file_path = os.path.dirname(script_path)  + filename
-
-                            # Save the image as a JPG file
-                            with open(file_path, 'wb') as file:
-                                file.write(image_bytes)
-
-                            print(f"Image saved successfully as {file_path}.")
-                        else:
-                            print("Generated image data not found for an image.")
-                else:
-                    print("No generated images found in the response.")
-                    print("Response content:", response.content.decode())
-            except ValueError as e:
-                print("Error parsing the response JSON:", str(e))
-        else:
-            print("Error:", response.text)
+        filename = response['choices'][0]['message']['content']
+        print(filename)
+        title = 'Your Poster'
     else:
-        image = Image.open(io.BytesIO(file))
-        answers = stability_api.generate(
-            prompt = text,
-            init_image=image.resize((512,512)),
-            seed = 0,
-            steps=50, # defaults to 30 if not specified
-            start_schedule=0.5,
+        thought = request.form.get('thought')
+        string = thought
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system", "content": "You are a helpful assistant which gernarate prompt in json format."},
+                {"role": "user", "content": "homer simpson archer"},
+                {"role": "assistant", "content": '''{"title":"shooting archer",
+    "prompt":"portait of a homer simpson archer shooting arrow at forest monster, front game card, drark, marvel comics, dark, intricate, highly detailed, smooth, artstation, digital illustration"}'''},
+                {"role": "user", "content": "pirate"},
+                {"role": "assistant", "content": '''{"title":"horrible pirate",
+    "prompt":"pirate, concept art, deep focus, fantasy, intricate, highly detailed, digital painting, artstation, matte, sharp focus, illustration"}'''},
+                {"role": "user", "content":string},
+            ]
         )
-        for resp in answers:
-            for artifact in resp.artifacts:
-                if artifact.finish_reason == generation.FILTER:
-                    warnings.warn(
-                        "Your request activated the API's safety filters and could not be processed."
-                        "Please modify the prompt and try again.")
-                if artifact.type == generation.ARTIFACT_IMAGE:
-                    img = Image.open(io.BytesIO(artifact.binary))
-        rgb_im = img.convert("RGB")
-        localtime = time.localtime()
-        result = time.strftime("%Y%m%d%I%M%S%p", localtime)
-        script_path = os.path.abspath( __file__ )
-        filename = "/static/images/" + str(result) + ".jpg"
-        rgb_im.save(os.path.dirname(script_path)  + filename)
-    return filename
+        json_string = response['choices'][0]['message']['content']
+        data = json.loads(json_string)
+        title = data["title"]
+        prompt = data["prompt"]
+        if flag == '0':
+            file = request.files['file'].read()
+            url = "https://api.mokker.ai/v1/replace-background"
+            files = {
+                "image": file
+            }
+            payload = {
+                "pos_prompt": prompt,
+                "number_of_images": "1"
+            }
+            headers = {
+                "Authorization": "Bearer " + MOKKER_KEY,
+                "accept": "application/json"
+            }
+            response = requests.post(
+            url = url,
+            headers = headers,
+            data = payload,
+            files = files
+            )
+            if response.status_code == 200:
+                # Retrieve the generated image data
+                try:
+                    # Retrieve the generated images list
+                    images = response.json().get('images', [])
+
+                    if images:
+                        for image in images:
+                            # Retrieve the generated image data
+                            image_data = image.get('image', {}).get('data')
+
+                            if image_data:
+                                # Decode the base64 image data
+                                image_bytes = base64.b64decode(image_data)
+
+                                # Specify the file path and name
+                                script_path = os.path.abspath( __file__ )
+                                localtime = time.localtime()
+                                result = time.strftime("%Y%m%d%I%M%S%p", localtime)
+                                filename = "/static/images/" + str(result) + ".jpg"
+                                file_path = os.path.dirname(script_path)  + filename
+
+                                # Save the image as a JPG file
+                                with open(file_path, 'wb') as file:
+                                    file.write(image_bytes)
+
+                                print(f"Image saved successfully as {file_path}.")
+                            else:
+                                print("Generated image data not found for an image.")
+                    else:
+                        print("No generated images found in the response.")
+                        print("Response content:", response.content.decode())
+                except ValueError as e:
+                    print("Error parsing the response JSON:", str(e))
+            else:
+                print("Error:", response.text)
+        else:
+            answers = stability_api.generate(
+                prompt = prompt,
+                steps=50, # defaults to 30 if not specified
+            )
+            for resp in answers:
+                for artifact in resp.artifacts:
+                    if artifact.finish_reason == generation.FILTER:
+                        warnings.warn(
+                            "Your request activated the API's safety filters and could not be processed."
+                            "Please modify the prompt and try again.")
+                    if artifact.type == generation.ARTIFACT_IMAGE:
+                        img = Image.open(io.BytesIO(artifact.binary))
+            rgb_im = img.convert("RGB")
+            localtime = time.localtime()
+            result = time.strftime("%Y%m%d%I%M%S%p", localtime)
+            script_path = os.path.abspath( __file__ )
+            filename = "/static/images/" + str(result) + ".jpg"
+            rgb_im.save(os.path.dirname(script_path)  + filename)
+
+    return {'filename': filename, 'title': title}
     
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,34 @@
 
 const dropbox = document.getElementById("upload-zone");
 const preview = document.getElementById("preview");
-
+var thoughtInput = document.getElementById("rand");
+thoughtInput.style.display = "none"
+document.getElementById("randtext").style.display = "none"
+document.getElementById("load").style.display = "none"
+document.getElementById("over").style.display = "none"
 var globalFlag = 0;
+var randButton = document.getElementById("rand");
 
+randButton.addEventListener("click", function() {
+  // 執行 submit 按鈕的相應操作
+  showLoading();
+  const formData1 = new FormData();
+  formData1.append("flag", 2);
+  fetch("/", {
+    method: "POST",
+    body: formData1
+  }).then(function(response) {
+    return response.json(); // Parse response body as JSON
+  }).then(function(data) {
+    document.getElementById("load").style.display = "none"
+    document.getElementById("over").style.display = "none"
+    const submitButton = document.getElementById("submit");
+    submitButton.disabled = false;
+    var randtextDiv = document.getElementById("randtext")
+  // Replace the content of the <div> with the replacement text
+    randtextDiv.innerHTML = data.filename;
+  });
+});
 // Get the "Poster Maker" choice element
 const posterMakerChoice = document.getElementById('posterd');
 
@@ -12,7 +37,14 @@ posterMakerChoice.addEventListener('click', function() {
   // Set the global flag to 1
   globalFlag = 0;
   const exampleElement = document.getElementById("example");
+  var thoughtInput = document.getElementById("rand");
+  thoughtInput.style.display = "none"
+  document.getElementById("randtext").style.display = "none"
+  dropbox.style.display = "block";
   exampleElement.innerText = "Poster Maker!";
+  var thoughtInput = document.getElementById("thought");
+  thoughtInput.value = "";
+  thoughtInput.placeholder = "2.請輸入期望背景長相";
 },false);
 const Choice = document.getElementById('productd');
 
@@ -21,6 +53,15 @@ Choice.addEventListener('click', function() {
   // Set the global flag to 1
   globalFlag = 1;
   const exampleElement = document.getElementById("example");
+  var thoughtInput = document.getElementById("thought");
+  thoughtInput.value = "";
+  thoughtInput.placeholder = "1.請輸入期望商品長相";
+  var thoughtInput = document.getElementById("rand");
+  document.getElementById("randtext").style.display = "block"
+  thoughtInput.style.display = "block"
+  dropbox.style.display = "none";
+  preview.innerHTML = ""; // 清空內容
+  preview.style.display = "none";
   exampleElement.innerText = "Product Designer!";
 },false);
 
@@ -73,6 +114,7 @@ function drop(e) {
     handleFiles(files);
     dropbox.classList.remove("upload-zone--enter");
     dropbox.style.display = "none";
+    preview.style.display = "block";
 }
 
 dropbox.addEventListener("click", click, false);
@@ -83,11 +125,9 @@ dropbox.addEventListener("drop", drop, false);
 function showLoading() {
     const submitButton = document.getElementById("submit");
     submitButton.disabled = true;
-    
+    document.getElementById("load").style.display = "block"
+    document.getElementById("over").style.display = "block"
     // Create the loading element
-    const loading = document.createElement('div');
-    loading.classList.add('loading');
-    document.body.appendChild(loading);
   }
 document.getElementById("upload_form").addEventListener("submit", function (e){
     e.preventDefault();
@@ -95,20 +135,58 @@ document.getElementById("upload_form").addEventListener("submit", function (e){
     const thought = document.getElementById("thought").value;
     const fileInput = document.getElementById("fileUploader");
     const file = fileInput.files[0];
-
     const formData = new FormData();
     formData.append("thought", thought);
-    formData.append("file", file);
+    if (globalFlag === 0) {
+      // Code to be executed if globalFlag is equal to 0
+      formData.append("file", file);
+      // Additional code here...
+    }
     formData.append("flag", globalFlag);
     fetch("/", {
         method: "POST",
         body: formData
       }).then(function(response) {
-        return response.text(); // Get response body as text
-      }).then(function(filename) {
+        return response.json(); // Parse response body as JSON
+      }).then(function(data) {
+        document.getElementById("load").style.display = "none"
+        document.getElementById("over").style.display = "none"
         const submitButton = document.getElementById("submit");
         submitButton.disabled = false;
         const poster = document.querySelector(".poster__img");
-        poster.setAttribute("src",  filename);
+        poster.setAttribute("src",  data.filename);
+        const randtextDiv = document.getElementById("instr");
+        randtextDiv.innerHTML = data.title;
       });
+});
+var choices = document.getElementsByClassName('choice');
+
+// 監聽鼠標移動事件
+document.addEventListener('mousemove', function(event) {
+  // 獲取當前鼠標的座標
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+
+  // 迭代所有 "choice" 元素
+  for (var i = 0; i < choices.length; i++) {
+    var choice = choices[i];
+    
+    // 獲取 "choice" 元素的位置和尺寸
+    var rect = choice.getBoundingClientRect();
+    var choiceX = rect.left;
+    var choiceY = rect.top;
+    var choiceWidth = rect.width;
+    var choiceHeight = rect.height;
+
+    // 檢查鼠標是否在 "choice" 元素的範圍內
+    if (mouseX >= choiceX && mouseX <= choiceX + choiceWidth &&
+        mouseY >= choiceY && mouseY <= choiceY + choiceHeight) {
+      // 在 "choice" 元素範圍內，將顏色變深一點
+      choice.style.filter = 'brightness(85%)';
+      choice.style.cursor = 'pointer';
+    } else {
+      // 不在 "choice" 元素範圍內，恢復原始顏色
+      choice.style.filter = 'none';
+    }
+  }
 });
